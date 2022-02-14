9eaf4953b4ba6cb31700051829746ca2fb70bf8f
Furkan Yıldız <furkanyildiz58.fy@gmail.com>
2022-02-14 15:15:29 +0300

    dasdasda

--- src/Furkanprofilecard.js
+++ src/Furkanprofilecard.js
@@ -5,6 +5,9 @@ const Furkanprofilecard = ({name,text,location,links}) => {
 
   return(
    <div>
+
+
+     
       <div className="wrapper">
     <div className="profile-card js-profile-card">
       <div className="profile-card__img">
